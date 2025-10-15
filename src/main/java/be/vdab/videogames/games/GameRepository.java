b3576b191907667d093945e4dd9cd189d1d84ed2
NickVandenbroeck <nick.vandenbroeck@vdabcampus.be>
2025-10-15 11:42:26 +0200

    Access modifiers verstrengd

--- src/main/java/be/vdab/videogames/games/GameRepository.java
+++ src/main/java/be/vdab/videogames/games/GameRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
+interface GameRepository extends JpaRepository<Game, Long> {
 
     @Override
     @EntityGraph(attributePaths = "consoles")
