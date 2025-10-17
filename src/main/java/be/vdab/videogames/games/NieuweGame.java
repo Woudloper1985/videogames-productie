0dfd655a6a54e61dbbaf97177983a14ffa97ced2
NickVandenbroeck <nick.vandenbroeck@vdabcampus.be>
2025-10-17 12:06:07 +0200

    JUnit: GameControllerTest 1.0

--- src/main/java/be/vdab/videogames/games/NieuweGame.java
+++ src/main/java/be/vdab/videogames/games/NieuweGame.java
@@ -9,7 +9,8 @@ record NieuweGame(
         @NotBlank String title,
         @NotBlank String developer,
         @NotNull @PastOrPresent LocalDate releaseDate,
-        @NotEmpty Set<@Positive Long> consoleIds, // minstens één console verplicht
-        @NotNull Genre genre //genre verplicht in te voeren
+        @NotNull Genre genre, //genre verplicht in te voeren
+        @NotEmpty Set<@Positive Long> consoleIds // minstens één console verplicht
+
 ) {
 }
